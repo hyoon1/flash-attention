21cc77afafb76e324cc5015847dc3d434813e256
Hosang Yoon <hosang.yoon@amd.com>
2026-01-28 16:07:58 +0000

    update default CTA config

--- run_tflops.sh
+++ run_tflops.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#TORCH_ROCM_AOTRITON_ENABLE_EXPERIMENTAL=1 FLASH_ATTN_LOG_DISABLE=1 python measure_flash_attn_tflops.py --sdpa --plot
+TORCH_ROCM_AOTRITON_ENABLE_EXPERIMENTAL=1 FLASH_ATTN_LOG_DISABLE=1 python measure_flash_attn_tflops.py --sdpa --plot
 
 ## gfx11
-TORCH_ROCM_AOTRITON_ENABLE_EXPERIMENTAL=1 FLASH_ATTN_LOG_DISABLE=1 FLASH_ATTENTION_TRITON_AMD_ENABLE=TRUE python measure_flash_attn_tflops.py --backend triton --sdpa --plot
+#TORCH_ROCM_AOTRITON_ENABLE_EXPERIMENTAL=1 FLASH_ATTN_LOG_DISABLE=1 FLASH_ATTENTION_TRITON_AMD_ENABLE=TRUE python measure_flash_attn_tflops.py --backend triton --sdpa --plot
 
